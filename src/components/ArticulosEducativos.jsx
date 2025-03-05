9232fffd2866716cada5d15bf485fe6cfaae412e
CRT-Dev21 <cesarromtor@gmail.com>
2025-03-04 22:33:31 -0600

    Corregir rutas de imágenes

--- src/components/ArticulosEducativos.jsx
+++ src/components/ArticulosEducativos.jsx
@@ -2,28 +2,28 @@ export const ArticulosEducativos = () => {
   const articulos = [
     {
       id: 1,
-      imagen: "./images/ecuacion.png", 
+      imagen: "https://CRT-Dev21.github.io/sitio-web-educativo/images/ecuacion.png", 
       titulo: "Cómo resolver ecuaciones de primer grado",
       descripcion: "Aprende a resolver ecuaciones de primer grado paso a paso.",
       enlace: "https://blogs.ugto.mx/bachilleratovirtual/clase-digital-4-ecuaciones-de-primer-grado/",
     },
     {
       id: 2,
-      imagen: "./images/agua_naturaleza.jpg",
+      imagen: "https://CRT-Dev21.github.io/sitio-web-educativo/images/agua_naturaleza.jpg",
       titulo: "El ciclo del agua: Explicación y ejemplos",
       descripcion: "Descubre cómo funciona el ciclo del agua en la naturaleza.",
       enlace: "https://concepto.de/ciclo-del-agua/",
     },
     {
       id: 3,
-      imagen: "./images/revolucion.jpg",
+      imagen: "https://CRT-Dev21.github.io/sitio-web-educativo/images/revolucion.jpg",
       titulo: "La Revolución Mexicana: Causas y consecuencias",
       descripcion: "Un análisis detallado de este importante evento histórico.",
       enlace: "https://edomex.gob.mx/revolucion_mexicana_2021",
     },
     {
       id: 4,
-      imagen: "./images/soledad.jpg",
+      imagen: "https://CRT-Dev21.github.io/sitio-web-educativo/images/soledad.jpg",
       titulo: "Análisis de 'Cien años de soledad'",
       descripcion: "Explora los temas y simbolismos de esta obra maestra.",
       enlace: "https://www.culturagenial.com/es/cien-anos-de-soledad-de-gabriel-garcia-marquez/",
